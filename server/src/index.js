dba2303b9e3bbb4b4faa2c95a124e18bde84f192
mdg0410 <levoyersteven@gmail.com>
2025-03-25 13:10:53 -0500

    Eliminar el registro de la URI de MongoDB en la consola

--- server/src/index.js
+++ server/src/index.js
@@ -10,8 +10,6 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 
-console.log(process.env.MONGO_URI);
-
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected'))
