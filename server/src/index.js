328a3daadff0b61c1a63365c3f93011f81da9bdf
mdg0410 <levoyersteven@gmail.com>
2025-03-25 13:10:04 -0500

    Actualizar scripts de npm y simplificar conexión a MongoDB

--- server/src/index.js
+++ server/src/index.js
@@ -13,10 +13,7 @@ app.use(express.json());
 console.log(process.env.MONGO_URI);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
 
